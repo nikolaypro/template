78000412c8f3c1e90ec8d636683f15490517b74e
nikolaypro <nikolay.prokopov@magenta-technology.com>
2016-11-23 20:09:54 +0400

    typeahead refactoring

--- src/web/src/main/webapp/app/template/typeahead/typeahead.js
+++ src/web/src/main/webapp/app/template/typeahead/typeahead.js
@@ -5,50 +5,28 @@
         .module('app')
         .directive('typeAhead', TypeAhead);
 
-    TypeAhead.$inject = ['$log', '$timeout', '$filter'];
+    TypeAhead.$inject = ['$log', '$timeout', '$filter', '$q'];
 
-    function TypeAhead($log, $timeout, $filter) {
+    function TypeAhead($log, $timeout, $filter, $q) {
         return {
             templateUrl: 'app/template/typeahead/typeahead.html',
             restrict: 'E',
-           transclude: true,
+            transclude: true,
 //             replace: true,
             scope: {
                 vm: '=info',
-                modelValue: '=',
+                ngModel: '=',
                 loadItems: '='
             },
             // require:"ngModel",
             link: function(scope, element, attrs, controller) {
                 //http://suhairhassan.com/2013/05/01/getting-started-with-angularjs-directive.html#.WBpEIGqLSUl
                 var vm = scope.vm;
-                var formInput = angular.element(element[0].firstChild.firstChild.nextElementSibling).controller("ngModel");
+//                var formInput = angular.element(element[0].firstChild.firstChild.nextElementSibling).controller("ngModel");
                 // angular.element(element[0].firstChild.firstChild.nextElementSibling).controller("uibTypeahead")
-                vm.items = null;
-                vm.getItems = function (str) {
-                    if (vm.items == null) {
-                        vm.loadingItems = true;
-                        return scope.loadItems(function (data) {
-                            $log.info("loadItems");
-                            vm.items = data;
-                            vm.loadingItems = false;
-                            // var formInput = vm.editForm[vm.elementId];
-                            $timeout(function () {
-                                var v = formInput.$viewValue;
-                                formInput.$setViewValue('');
-                                $timeout(function () {
-                                    formInput.$setViewValue(v);
-                                }, 0);
-                            }, 0);
-                            return vm.filter(str);
-                        });
-                    }
-                    return vm.filter(str);
-                };
-
-                vm.filter = function(str) {
+                var filter = function(str, items) {
                     var result = [];
-                    angular.forEach(vm.items, function(item) {
+                    angular.forEach(items, function(item) {
                         if (typeof str == 'undefined' || (item.name).toLowerCase().indexOf(('' + str).toLowerCase()) > -1) {
                             result.push(item);
                         }
@@ -56,10 +34,25 @@
                     // $log.info("Str = " + str + ", Filtered: " + result);
                     return result;
                 };
+                scope.items = null;
+                scope.getItems = function (str) {
+                    if (scope.items == null) {
+                        scope.loadingItems = true;
+                        var deferred = $q.defer();
+                        scope.loadItems(function(data) {
+                            scope.items = data;
+                            scope.loadingItems = false;
+                            deferred.resolve(filter(str, data));
+                        });
+                        return deferred.promise;
+                    }
+                    return filter(str, scope.items);
+                };
 
-                vm.onSelectItem = function(item) {
+
+                scope.onSelectItem = function(item) {
                     $log.info("Selected: " + item);
-                    scope.modelValue = item;
+                    scope.ngModel = item;
                 };
 
                 scope.getPrefix = function(item) {
@@ -70,9 +63,9 @@
                     return '[' + vm.getComboItemPrefix(item) + ']';
                 };
 
-                vm.isCurrent = function(item) {
-                    // $log.info("AAAAAAA: " + (item == scope.modelValue));
-                    return item == scope.modelValue;
+                scope.isCurrent = function(item) {
+                    // $log.info("AAAAAAA: " + (item == scope.ngModel));
+                    return item == scope.ngModel;
                 }
 
             }
