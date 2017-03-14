be985ea17efe9814f55b0e5c430604f78413104f
nikolaypro <nikolay.prokopov@magenta-technology.com>
2017-03-14 11:17:13 +0400

    add integration user

--- src/service/src/main/java/com/mascot/service/controller/integration/IntegrationController.java
+++ src/service/src/main/java/com/mascot/service/controller/integration/IntegrationController.java
@@ -1,9 +1,11 @@
 package com.mascot.service.controller.integration;
 
 import com.mascot.server.beans.UserService;
+import com.mascot.server.model.Role;
 import com.mascot.service.controller.AbstractController;
 import com.mascot.service.controller.integration.dto.IntegrationResponse;
 import com.mascot.service.controller.integration.dto.ExtUser;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
@@ -21,6 +23,7 @@ public class IntegrationController extends AbstractController {
 
     @RequestMapping(value = "/users", method = RequestMethod.POST)
     @ResponseBody
+    @PreAuthorize("hasRole('" + Role.ADMIN + "')")
     public IntegrationResponse processUsers(@RequestBody List<ExtUser> extUsers) {
         logger.info("integration/users!! Size: " + extUsers.size());
         return IntegrationResponse.SUCCESS;
